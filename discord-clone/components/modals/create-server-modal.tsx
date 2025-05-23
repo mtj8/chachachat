298ca0397ea3a429fa4582fc6a0b04a94eef9630
Matt-Joshua Tan <mattjoshuatan@gmail.com>
2025-05-22 21:06:51 -0400

    delete and leave server modals

--- discord-clone/components/modals/create-server-modal.tsx
+++ discord-clone/components/modals/create-server-modal.tsx
@@ -104,6 +104,7 @@ export const CreateServerModal = () => {
                                                     onChange={field.onChange}
                                                 />
                                             </FormControl>
+                                            <FormMessage />
                                         </FormItem>
                                     )}
                                 />
