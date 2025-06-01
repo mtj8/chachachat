dbe0c178d36c65214f4ddb27ca0d3e26221263ae
Matt-Joshua Tan <mattjoshuatan@gmail.com>
2025-06-01 18:18:12 -0400

    another eslint fix 2

--- discord-clone/eslint.config.mjs
+++ discord-clone/eslint.config.mjs
@@ -13,8 +13,8 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "no-explicit-any": "off",
-      "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   }),
 ];
